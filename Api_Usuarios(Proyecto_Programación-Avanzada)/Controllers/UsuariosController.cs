0f182049db06e3d30757d36747f0e7cc01a58243
Sebastian_yrb <123045887+ReyesSebass@users.noreply.github.com>
2024-11-30 15:16:11 -0600

    Se finazlizo el controller de Usuarios

--- Api_Usuarios(Proyecto_Programación-Avanzada)/Controllers/UsuariosController.cs
+++ Api_Usuarios(Proyecto_Programación-Avanzada)/Controllers/UsuariosController.cs
@@ -1,83 +1,91 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Api_Usuarios_Proyecto_Programación_Avanzada_.Data;
+using Api_Usuarios_Proyecto_Programación_Avanzada_.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api_Usuarios_Proyecto_Programación_Avanzada_.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class UsuariosController : Controller
     {
-        // GET: UsuariosController
-        public ActionResult Index()
+        private readonly ConexionDbContext _contextAcceso;
+
+        public UsuariosController(ConexionDbContext contextAcceso)
         {
-            return View();
+            _contextAcceso = contextAcceso;
         }
 
-        // GET: UsuariosController/Details/5
-        public ActionResult Details(int id)
+        //Mostrar todos los usuarios existentes
+        [HttpGet]
+        public ActionResult<IEnumerable<UsuarioModel>> ObtenerUsuarios()
         {
-            return View();
+            return Ok(_contextAcceso.users.ToList());
         }
 
-        // GET: UsuariosController/Create
-        public ActionResult Create()
+        // Obtener un usuario por ID
+        [HttpGet("{_id}")]
+        public ActionResult<IEnumerable<UsuarioModel>> ObtenerUsuarios(int _id)
         {
-            return View();
+            var datos = _contextAcceso.users.Find(_id);
+
+            if (datos == null)
+            {
+                return NotFound("El dato buscado no existe.");
+            }
+
+            return Ok(datos);
         }
 
-        // POST: UsuariosController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        //Editar/Modificar un usuario existente
+        [HttpPut]
+        public IActionResult ModificarUsuario(UsuarioModel _datos)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (!ConsultarDatos(_datos.user_id))
+                {
+                    return NotFound("El dato buscado no existe.");
+                }
+                _contextAcceso.Entry(_datos).State = EntityState.Modified;
+                _contextAcceso.SaveChanges();
+
+                return Ok("Usuario modificado exitosamente.");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                return StatusCode(500, ex.Message);
             }
         }
 
-        // GET: UsuariosController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: UsuariosController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UsuariosController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: UsuariosController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        //Eliminar un usuarios
+        [HttpDelete("{_id}")]
+        public ActionResult EliminarUsuarios(int _id)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (!ConsultarDatos(_id))
+                {
+                    return NotFound("El dato buscado no existe.");
+                }
+                var datos = _contextAcceso.users.Find(_id);
+                _contextAcceso.users.Remove(datos);
+                _contextAcceso.SaveChanges();
+
+                return Ok($"Se elimino el registro {_id}");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                return StatusCode(500, ex.Message);
             }
+        }
+
+
+        // Funcion para verificar si exise 
+        private bool ConsultarDatos(int _id)
+        {
+            return _contextAcceso.users.Any(x => x.user_id == _id);
         }
     }
 }
